7947c1463dccf9bb9b7f71414437e34f0ce9fe75
TheCodeDr <43827099+TheCodeDr@users.noreply.github.com>
2018-10-03 15:33:29 -0400

    Updated
    New Netscript documentation fixes for daemon and rent scripts

--- daemon.js
+++ daemon.js
@@ -59,13 +59,13 @@ hackThreadHardening = 0.002;
 weakenThreadPotency = 0.05 * bitnodeWeakenMult;
 
 // hacking target requires 1.50GB of RAM to run for 1 thread(s)
-hackCost = 1.5;
+hackCost = 2.45;
 
-// weaken-target.script requires 1.55GB of RAM to run for 1 thread(s)
-weakenCost = 1.555;
+// weaken-target.script requires 1.75GB of RAM to run for 1 thread(s)
+weakenCost = 1.75;
 
-// grow-target.script requires 1.55GB of RAM to run for 1 thread(s)
-growCost = 1.555;
+// grow-target.script requires 1.75GB of RAM to run for 1 thread(s)
+growCost = 1.75;
 
 // one-time scheduler cost per cycle
 schedulerCost = 2.50 * 2;
@@ -86,7 +86,7 @@ killDelay = 8;
 //if previous daemons were running, this kills all their child scripts
 scriptsToCull = ['weaken-target.script', 'grow-target.script', 'hack-target.script'];
 for (i = 0; i < scriptsToCull.length; i++) {
-  scriptKill(scriptsToCull[i], hostName);
+    scriptKill(scriptsToCull[i], hostName);
 }
 
 //according to chapt3r, it shouldn't take terribly long for all kills to finish terminating existing scripts - we sleep here just in case
@@ -97,121 +97,121 @@ sleep(killDelay * 1000, false);
 doLoop = true;
 
 while (doLoop) {
-  hackingLevel = getHackingLevel();
-  currentSecurity = getServerSecurityLevel(target);
+    hackingLevel = getHackingLevel();
+    currentSecurity = getServerSecurityLevel(target);
 
-  if (Math.floor(currentSecurity) > Math.floor(minSecurity)) {
-    //execution times based on current security, how long to sleep, since we're using all available RAM to weaken target
-    weakenExecutionTime = getWeakenTime(target);
-    weakenExecutionTime = round(weakenExecutionTime * 1000) / 1000;
+    if (Math.floor(currentSecurity) > Math.floor(minSecurity)) {
+        //execution times based on current security, how long to sleep, since we're using all available RAM to weaken target
+        weakenExecutionTime = getWeakenTime(target);
+        weakenExecutionTime = Math.round(weakenExecutionTime * 1000) / 1000;
 
-    threadsNeeded = Math.ceil((currentSecurity - minSecurity) / weakenThreadPotency);
-    ramAvailableArray = getServerRam(hostName);
-    ramAvailable = ramAvailableArray[0] - ramAvailableArray[1];
-    threadsUsed = Math.floor(ramAvailable / weakenCost);
+        threadsNeeded = Math.ceil((currentSecurity - minSecurity) / weakenThreadPotency);
+        ramAvailableArray = getServerRam(hostName);
+        ramAvailable = ramAvailableArray[0] - ramAvailableArray[1];
+        threadsUsed = Math.floor(ramAvailable / weakenCost);
 
-    //this causes the script to pass through this cycle if it can't weaken, causing it to idle until some RAM is free.
-    if (threadsNeeded > 0 && threadsUsed > 0) {
-      tprint('[' + hostName + '] ' + 'Server ' + target + ' is being weakened...');
-      run('weaken-target.script', threadsUsed, target);
-      delay = (weakenExecutionTime + activationDelay + killDelay);
+        //this causes the script to pass through this cycle if it can't weaken, causing it to idle until some RAM is free.
+        if (threadsNeeded > 0 && threadsUsed > 0) {
+            tprint('[' + hostName + '] ' + 'Server ' + target + ' is being weakened...');
+            run('weaken-target.script', threadsUsed, target);
+            delay = (weakenExecutionTime + activationDelay + killDelay);
 
-      sleep(delay * 1000, false);
-    }
-  } else {
-    adjGrowthRate = 1 + ((unadjustedGrowthRate - 1) / minSecurity);
-    adjGrowthRate = Math.min(maxGrowthRate, adjGrowthRate);
-    serverGrowthPercentage = constantGrowthRate / 100;
-    numServerGrowthCyclesAdjusted = serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult;
-    serverGrowth = Math.pow(adjGrowthRate, numServerGrowthCyclesAdjusted);
-
-    neededToMaxInitially = maxMoney / Math.max(getServerMoneyAvailable(target), 1);
-
-    //here we presume that 1 / (percentageToHack) is the actual coefficient to achieve our "recovery" growth each theft.
-    neededToMax = 1 / (1 - percentageToSteal); //maxMoney / Math.max(getServerMoneyAvailable(target), 1);
-
-    //this is the cycles needed not accounting for growth mults (bitnode/player) and growthPercentage yet.
-    cyclesNeededToGrowInitially = Math.log(neededToMaxInitially) / Math.log(adjGrowthRate);
-    cyclesNeededToGrow = Math.log(neededToMax) / Math.log(adjGrowthRate);
-
-    //since the player growth mult and bitnode mult are applied to the *exponent* of the growth formula
-    //this pulls them back out. serverGrowthPercentage ends up being a multiplier for threads needed in this case.
-    threadsNeededToGrowInitially = Math.ceil(cyclesNeededToGrowInitially / (serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult));
-    totalGrowCostInitially = threadsNeededToGrowInitially * growCost;
-    threadsNeededToGrow = Math.ceil(cyclesNeededToGrow / (serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult));
-    totalGrowCost = threadsNeededToGrow * growCost;
-
-    //execution times based on min security, as a best guess for how much we can do in one weaken cycle.
-    weakenExecutionTime = getWeakenTime(target);
-    weakenExecutionTime = round(weakenExecutionTime * 1000) / 1000;
-
-    growExecutionTime = getGrowTime(target);
-    growExecutionTime = round(growExecutionTime * 1000) / 1000;
-
-    hackExecutionTime = getHackTime(target);
-    hackExecutionTime = round(hackExecutionTime * 1000) / 1000;
-
-    //one of the money multipliers, we base it off of min security, but we have to account for the offsets we've fired.
-    difficultyMult = (100 - Math.min(100, minSecurity)) / 100;
-
-    skillMult = (hackingLevel - (serverHackingLevel - 1)) / hackingLevel;
-    //difficulty mult is a constant based on min security, but skill mult is based on your current hacking level.
-    percentMoneyHacked = difficultyMult * skillMult * (playerHackingMoneyMult / 240);
-
-    //I can't imagine your hacking skills being this high but what the hell, it's part of the formula.
-    percentMoneyHacked = Math.min(1, Math.max(0, percentMoneyHacked));
-
-    threadsNeededToHack = Math.floor(percentageToSteal / percentMoneyHacked);
-    percentageToStealForDisplay = round(percentageToSteal * 100);
-    totalHackCost = (threadsNeededToHack * hackCost);
-
-    threadsNeededToWeakenForHack = (threadsNeededToHack * hackThreadHardening);
-    threadsNeededToWeakenForHack = Math.ceil(threadsNeededToWeakenForHack / weakenThreadPotency);
-    totalWeakenCostForHack = (threadsNeededToWeakenForHack * weakenCost);
-
-    threadsNeededToWeakenForGrow = (threadsNeededToGrow * growthThreadHardening);
-    threadsNeededToWeakenForGrow = Math.ceil(threadsNeededToWeakenForGrow / weakenThreadPotency);
-    totalWeakenCostForGrow = (threadsNeededToWeakenForGrow * weakenCost);
-
-    totalCostForAllCycles = totalHackCost + threadsNeededToWeakenForHack + totalGrowCost + totalWeakenCostForGrow + schedulerCost;
-    hostRamAvailable = getServerRam(hostName);
-
-    cyclesSupportedByRam = Math.floor((hostRamAvailable[0] - hostRamAvailable[1]) / totalCostForAllCycles);
-
-    skipHackDueToCycleImperfection = false;
-    if (weakenExecutionTime / windowDelay < cyclesSupportedByRam && percentageToSteal < 0.9) { //max of 90%
-      print('[' + hostName + '] ' + 'Based on ' + windowDelay.toString() + ' second window timing, percentage to steal of ' + percentageToStealForDisplay.toString() + ' is too low. Adjusting for next run-loop.');
-      percentageToSteal += 0.1;
-      skipHackDueToCycleImperfection = true;
-    } else if (cyclesSupportedByRam === 0 && percentageToSteal > 0.02) { //minimum of 2%
-      print('[' + hostName + '] ' + 'Current percentage to steal of ' + percentageToStealForDisplay.toString() + ' is too high for even 1 cycle. Adjusting for next run-loop.')
-      percentageToSteal -= 0.01;
-      skipHackDueToCycleImperfection = true;
-    }
-
-    if (threadsNeededToGrowInitially > 0) {
-      threadsAvailableToGrow = Math.floor((hostRamAvailable[0] - hostRamAvailable[1]) / growCost);
-      run('grow-target.script', threadsAvailableToGrow, target);
-      tprint('[' + hostName + '] ' + 'Server ' + target + ' is being grown...');
-      delay = (growExecutionTime + activationDelay + killDelay);
-      sleep(delay * 1000, false);
-    } else {
-      //pass over this run so that the script can obtain a better cycle estimation.
-      if (!skipHackDueToCycleImperfection) {
-        tprint('[' + hostName + '] ' + target + ' --- Hack to ' + percentageToStealForDisplay.toString() + '%' + ' x ' + cyclesSupportedByRam.toString() + ' cycles with a weaken execution time of ' + weakenExecutionTime.toString());
-        for (i = 0; i < cyclesSupportedByRam; i++) {
-          scripts = ['hack-scheduler.script', 'grow-scheduler.script'];
-          threadsNeededForWeaken = [threadsNeededToWeakenForHack, threadsNeededToWeakenForGrow];
-          threadsNeeded = [threadsNeededToHack, threadsNeededToGrow];
-          executionTime = [hackExecutionTime, growExecutionTime];
-          for (j = 0; j < scripts.length; j++) {
-            run(scripts[j], 1, target, threadsNeededForWeaken[j] * cyclesSupportedByRam, threadsNeeded[j] * cyclesSupportedByRam, weakenExecutionTime, executionTime[j], i);
-            sleep(stepDelay * 1000, false);
-          }
-          i += cyclesSupportedByRam
+            sleep(delay * 1000, false);
         }
-        sleep((weakenExecutionTime + activationDelay + killDelay) * 1000, false);
-      }
+    } else {
+        adjGrowthRate = 1 + ((unadjustedGrowthRate - 1) / minSecurity);
+        adjGrowthRate = Math.min(maxGrowthRate, adjGrowthRate);
+        serverGrowthPercentage = constantGrowthRate / 100;
+        numServerGrowthCyclesAdjusted = serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult;
+        serverGrowth = Math.pow(adjGrowthRate, numServerGrowthCyclesAdjusted);
+
+        neededToMaxInitially = maxMoney / Math.max(getServerMoneyAvailable(target), 1);
+
+        //here we presume that 1 / (percentageToHack) is the actual coefficient to achieve our "recovery" growth each theft.
+        neededToMax = 1 / (1 - percentageToSteal); //maxMoney / Math.max(getServerMoneyAvailable(target), 1);
+
+        //this is the cycles needed not accounting for growth mults (bitnode/player) and growthPercentage yet.
+        cyclesNeededToGrowInitially = Math.log(neededToMaxInitially) / Math.log(adjGrowthRate);
+        cyclesNeededToGrow = Math.log(neededToMax) / Math.log(adjGrowthRate);
+
+        //since the player growth mult and bitnode mult are applied to the *exponent* of the growth formula
+        //this pulls them back out. serverGrowthPercentage ends up being a multiplier for threads needed in this case.
+        threadsNeededToGrowInitially = Math.ceil(cyclesNeededToGrowInitially / (serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult));
+        totalGrowCostInitially = threadsNeededToGrowInitially * growCost;
+        threadsNeededToGrow = Math.ceil(cyclesNeededToGrow / (serverGrowthPercentage * bitnodeGrowMult * playerHackingGrowMult));
+        totalGrowCost = threadsNeededToGrow * growCost;
+
+        //execution times based on min security, as a best guess for how much we can do in one weaken cycle.
+        weakenExecutionTime = getWeakenTime(target);
+        weakenExecutionTime = Math.round(weakenExecutionTime * 1000) / 1000;
+
+        growExecutionTime = getGrowTime(target);
+        growExecutionTime = Math.round(growExecutionTime * 1000) / 1000;
+
+        hackExecutionTime = getHackTime(target);
+        hackExecutionTime = Math.round(hackExecutionTime * 1000) / 1000;
+
+        //one of the money multipliers, we base it off of min security, but we have to account for the offsets we've fired.
+        difficultyMult = (100 - Math.min(100, minSecurity)) / 100;
+
+        skillMult = (hackingLevel - (serverHackingLevel - 1)) / hackingLevel;
+        //difficulty mult is a constant based on min security, but skill mult is based on your current hacking level.
+        percentMoneyHacked = difficultyMult * skillMult * (playerHackingMoneyMult / 240);
+
+        //I can't imagine your hacking skills being this high but what the hell, it's part of the formula.
+        percentMoneyHacked = Math.min(1, Math.max(0, percentMoneyHacked));
+
+        threadsNeededToHack = Math.floor(percentageToSteal / percentMoneyHacked);
+        percentageToStealForDisplay = Math.round(percentageToSteal * 100);
+        totalHackCost = (threadsNeededToHack * hackCost);
+
+        threadsNeededToWeakenForHack = (threadsNeededToHack * hackThreadHardening);
+        threadsNeededToWeakenForHack = Math.ceil(threadsNeededToWeakenForHack / weakenThreadPotency);
+        totalWeakenCostForHack = (threadsNeededToWeakenForHack * weakenCost);
+
+        threadsNeededToWeakenForGrow = (threadsNeededToGrow * growthThreadHardening);
+        threadsNeededToWeakenForGrow = Math.ceil(threadsNeededToWeakenForGrow / weakenThreadPotency);
+        totalWeakenCostForGrow = (threadsNeededToWeakenForGrow * weakenCost);
+
+        totalCostForAllCycles = totalHackCost + threadsNeededToWeakenForHack + totalGrowCost + totalWeakenCostForGrow + schedulerCost;
+        hostRamAvailable = getServerRam(hostName);
+
+        cyclesSupportedByRam = Math.floor((hostRamAvailable[0] - hostRamAvailable[1]) / totalCostForAllCycles);
+
+        skipHackDueToCycleImperfection = false;
+        if (weakenExecutionTime / windowDelay < cyclesSupportedByRam && percentageToSteal < 0.9) { //max of 90%
+            print('[' + hostName + '] ' + 'Based on ' + windowDelay.toString() + ' second window timing, percentage to steal of ' + percentageToStealForDisplay.toString() + ' is too low. Adjusting for next run-loop.');
+            percentageToSteal += 0.1;
+            skipHackDueToCycleImperfection = true;
+        } else if (cyclesSupportedByRam === 0 && percentageToSteal > 0.02) { //minimum of 2%
+            print('[' + hostName + '] ' + 'Current percentage to steal of ' + percentageToStealForDisplay.toString() + ' is too high for even 1 cycle. Adjusting for next run-loop.')
+            percentageToSteal -= 0.01;
+            skipHackDueToCycleImperfection = true;
+        }
+
+        if (threadsNeededToGrowInitially > 0) {
+            threadsAvailableToGrow = Math.floor((hostRamAvailable[0] - hostRamAvailable[1]) / growCost);
+            run('grow-target.script', threadsAvailableToGrow, target);
+            tprint('[' + hostName + '] ' + 'Server ' + target + ' is being grown...');
+            delay = (growExecutionTime + activationDelay + killDelay);
+            sleep(delay * 1000, false);
+        } else {
+            //pass over this run so that the script can obtain a better cycle estimation.
+            if (!skipHackDueToCycleImperfection) {
+                tprint('[' + hostName + '] ' + target + ' --- Hack to ' + percentageToStealForDisplay.toString() + '%' + ' x ' + cyclesSupportedByRam.toString() + ' cycles with a weaken execution time of ' + weakenExecutionTime.toString());
+                for (i = 0; i < cyclesSupportedByRam; i++) {
+                    scripts = ['hack-scheduler.script', 'grow-scheduler.script'];
+                    threadsNeededForWeaken = [threadsNeededToWeakenForHack, threadsNeededToWeakenForGrow];
+                    threadsNeeded = [threadsNeededToHack, threadsNeededToGrow];
+                    executionTime = [hackExecutionTime, growExecutionTime];
+                    for (j = 0; j < scripts.length; j++) {
+                        run(scripts[j], 1, target, threadsNeededForWeaken[j] * cyclesSupportedByRam, threadsNeeded[j] * cyclesSupportedByRam, weakenExecutionTime, executionTime[j], i);
+                        sleep(stepDelay * 1000, false);
+                    }
+                    i += cyclesSupportedByRam
+                }
+                sleep((weakenExecutionTime + activationDelay + killDelay) * 1000, false);
+            }
+        }
     }
-  }
 }
--- rent.js
+++ rent.js
@@ -1,30 +1,30 @@
 //1% of current funds, per cycle.
 allowancePercentage = 0.01;
 while (true) {
-  currentCash = getServerMoneyAvailable('home');
-  currentCash *= allowancePercentage;
-  if (getNextHacknetNodeCost() <= currentCash) {
-    purchaseHacknetNode();
-  } else {
-    for (i = 0; i < hacknetnodes.length; i++) {
-      node = hacknetnodes[i];
-      upgradeCost = node.getLevelUpgradeCost(1);
-      if (upgradeCost <= currentCash) {
-        node.upgradeLevel(1);
-        break;
-      } else {
-        ramCost = node.getRamUpgradeCost();
-        if (ramCost <= currentCash) {
-          node.upgradeRam();
-          break;
-        } else {
-          coreCost = node.getCoreUpgradeCost();
-          if (coreCost <= currentCash) {
-            node.upgradeCore();
-            break;
-          }
+    currentCash = getServerMoneyAvailable('home');
+    currentCash *= allowancePercentage;
+    if (hacknet.getPurchaseNodeCost() <= currentCash) {
+        hacknet.purchaseNode();
+    } else {
+        for (i = 0; i < hacknet.numNodes; i++) {
+            node = hacknetnodes[i];
+            upgradeCost = hacknet.getLevelUpgradeCost(1);
+            if (upgradeCost <= currentCash) {
+                hacknet.upgradeLevel(1);
+                break;
+            } else {
+                ramCost = hacknet.getRamUpgradeCost();
+                if (ramCost <= currentCash) {
+                    hacknet.upgradeRam();
+                    break;
+                } else {
+                    coreCost = hacknet.getCoreUpgradeCost();
+                    if (coreCost <= currentCash) {
+                        hacknet.upgradeCore();
+                        break;
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
